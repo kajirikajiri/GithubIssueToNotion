4514d479e01727a0669f368b8e89f5714160357e
kajirikajiri <kajirikajiri@gmail.com>
2021-10-11 02:23:16 +0900

    :sparkles: second commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
+# 作成中です
 # GithubIssueToNotion
+
+## must
+`1. Create a table using this template and copy the URL of the page.`
+https://kajirikajiri.notion.site/963ef208eda14cb592837a6c9b0d9be2?v=ab7609ff7de740fbb2d07708d70fadb9
+
+`2. Process the copied url.`
+https://YOUR_WORKSPACE.notion.site/12345678123412341234123456789012?v=22222222222222222222222222222222
+↓
+12345678123412341234123456789012
+↓
+12345678-1234-1234-1234-123456789012
+`3. Register Repository secrets`
+key: NOTION_DATABASE_ID
+value: Value created in `2. Process the copied url.` ex) 12345678-1234-1234-1234-123456789012
+
+`4. register Repository secrets to NOTION_INTERNAL_API_KEY`
+key: NOTION_INTERNAL_API_KEY
+value: Your Internal API KEY(https://www.notion.so/my-integrations)
+
+
+```
+name: CI
+on:
+  issues:
+    types: [opened, edited, closed, reopened, deleted]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:      
+      - uses: actions/GithubIssueToNotion@latest
+      - name: Github issue to Notion
+        uses: actions/GithubIssueToNotion@latest
+        with:
+          notion-internal-api-key: ${{ secrets.NOTION_INTERNAL_API_KEY }}
+          notion-database-id: ${{ secrets.NOTION_DATABASE_ID }}
+          issue-title: ${{ github.event.issue.title }}
+          issue-number: ${{ github.event.issue.number }}
+          issue-state: ${{ github.event.issue.state }}
+          issue-tags-url: ${{ github.event.issue.tags_url }}
+          issue-url: ${{ github.event.issue.html_url }}
+          number-of-comments: ${{ github.event.issue.comments }}
+```
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,108 @@
 import * as core from "@actions/core";
-import * as io from "@actions/io";
-import path from "path";
-import cp from "child_process";
-import fs from "fs";
-import { URL } from "url";
+import { Client } from "@notionhq/client";
+
+type IssueLabel = {
+  color: string;
+  default: boolean;
+  description: string;
+  id: number;
+  name: string;
+  node_id: string;
+  url: string;
+};
 
 export const run = () => {
   try {
-    const notionApiKey = core.getInput("notion-api-key");
-    const databaseId = core.getInput("database-id");
-    core.info("notionApiKey");
-    core.info("databaseId");
-    core.endGroup();
+    const {
+      issueLabels,
+      issueNumber,
+      issueState,
+      issueTitle,
+      issueUrl,
+      notionApiKey,
+      notionDatabaseId,
+    } = getInputList();
+    const notion = new Client({ auth: notionApiKey });
+    if (notionDatabaseId) {
+      notion.pages
+        .create({
+          parent: { database_id: notionDatabaseId },
+          properties: getPropertiesFromIssue(
+            issueTitle,
+            issueNumber,
+            issueState,
+            issueUrl,
+            issueLabels
+          ),
+        })
+        .then(() => {
+          core.endGroup();
+        })
+        .catch((e) => {
+          core.error(e);
+          core.endGroup();
+        });
+    } else {
+      core.endGroup();
+    }
   } catch (error: any) {
     core.setFailed(error.message);
   }
 };
+
+function getInputList() {
+  const notionApiKey = core.getInput("notion-internal-api-key");
+  const notionDatabaseId = core
+    .getInput("notion-database-page-url")
+    .split("/")[4]
+    ?.split("?")[0];
+  const issueTitle = core.getInput("issue-title");
+  const issueNumber = core.getInput("issue-number");
+  const issueState = core.getInput("issue-state");
+  const issueLabelsJson = core.getInput("issue-labels");
+  const issueLabels = JSON.parse(
+    issueLabelsJson.length ? issueLabelsJson : "[]"
+  );
+  const issueUrl = core.getInput("issue-url");
+  return {
+    notionApiKey,
+    notionDatabaseId,
+    issueTitle,
+    issueNumber,
+    issueState,
+    issueLabels,
+    issueUrl,
+  };
+}
+
+function getPropertiesFromIssue(
+  issueTitle: string,
+  issueNumber: string,
+  issueState: string,
+  issueUrl: string,
+  issueLabels: IssueLabel[]
+): any {
+  return {
+    Name: {
+      type: "title",
+      title: [{ type: "text", text: { content: issueTitle } }],
+    },
+    "Issue Number": {
+      type: "number",
+      number: Number(issueNumber),
+    },
+    State: {
+      type: "select",
+      select: { name: issueState },
+    },
+    "Issue URL": {
+      type: "url",
+      url: issueUrl,
+    },
+    Labels: {
+      multi_select: issueLabels.map((l) => {
+        return { name: l.name };
+      }),
+    },
+  };
+}
