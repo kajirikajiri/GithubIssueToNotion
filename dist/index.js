da773dddc12594955b82eac5b16295e539a4a2a8
kajirikajiri <kajirikajiri@gmail.com>
2021-10-11 23:38:10 +0900

    :sparkles: forth commit

--- dist/index.js
+++ dist/index.js
@@ -460,10 +460,20 @@ exports.run = void 0;
 const core = __importStar(__webpack_require__(470));
 const run = () => {
     try {
-        const notionApiKey = core.getInput('notion-api-key');
-        const databaseId = core.getInput('database-id');
-        core.info('notionApiKey');
-        core.info('databaseId');
+        const notionApiKey = core.getInput("notion-api-key");
+        const databaseId = core.getInput("database-id");
+        const issueTitle = core.getInput("issue-title");
+        const issueNumber = core.getInput("issue-number");
+        const issueState = core.getInput("issue-state");
+        const issueTagsUrl = core.getInput("issue-tags-url");
+        const issueUrl = core.getInput("issue-url");
+        const numberOfComments = core.getInput("number-of-comments");
+        core.info(issueTitle);
+        core.info(issueNumber);
+        core.info(issueState);
+        core.info(issueTagsUrl);
+        core.info(issueUrl);
+        core.info(numberOfComments);
         core.endGroup();
     }
     catch (error) {
