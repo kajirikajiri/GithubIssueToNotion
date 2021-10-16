7cecc7fb3f1ce395397f81c42a338933cf457d5a
kajirikajiri <kajirikajiri@gmail.com>
2021-10-16 23:34:04 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ export const run = () => {
     if (notionDatabaseId) {
       notion.pages
         .create({
-          parent: { database_id: notionDatabaseId },
+          parent: { database_id: "c77f194f-8e13-41c5-8ca2-ff83006d7c97" },
           properties: getPropertiesFromIssue(
             issueTitle,
             issueNumber,
